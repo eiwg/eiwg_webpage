9c7eabe120ffa3cd367d796b03b7f5c5fb9729c0
Your Name <you@example.com>
2024-07-06 18:30:17 +0200

    Update Subteams

--- tools/lead.r
+++ tools/lead.r
@@ -1,15 +1,13 @@
 lead <- function(subteam, type = 1, active = 1){
-  l1 <- (email %>% filter(Lead_TF == subteam))
+  l1 <- (email %>% filter(Lead == subteam))
   
-  if (active == 3){l1 <- (email %>% filter(Lead == subteam))}
-  
-  l2 <- as.data.frame(l1 %>% select(Firstname, Lastname, Institution))
+  l2 <- as.data.frame(l1 %>% select(Name, Company))
   
   if (type == 1){
-    for (k in 1:nrow(l2)){l2[k, 4] <- paste(paste(l2[k, 1:2], collapse = " "), " (", l2[k, 3], ")", sep = "")}}
+    for (k in 1:nrow(l2)){l2[k, 3] <- paste(paste(l2[k, 1], collapse = " "), " (", l2[k, 2], ")", sep = "")}}
 
   if (type == 2){
-    for (k in 1:nrow(l2)){l2[k, 4] <- paste(paste(l2[k, 1:2], collapse = " "), ", ", l2[k, 3], sep = "")}}
+    for (k in 1:nrow(l2)){l2[k, 3] <- paste(paste(l2[k, 1], collapse = " "), ", ", l2[k, 2], sep = "")}}
   
-  return(l2[, 4])
+  return(l2[, 3])
 }
